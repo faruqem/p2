a5969d3353dae12360d1f244e44635138b029e8e
Mohiuddin Faruqe <faruqem@yahoo.com>
2016-09-13 22:19:48 -0400

    Update index.php, incorporate bootstrap framework and add local CSS.

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<title>Project P2</title>
-</head>
-<body>
-	<h1>
-		<?php
-			echo "This is project P2.";
-		?>
-	</h1>
-</body>
+  <head>
+    <meta charset ="utf-8">
+    <title>Project P2</title>
+
+    <!--Bootstrap CDNs-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!--Local CSS file-->
+    <link rel="stylesheet" href="css/my_style.css">
+  <head>
+  <body>
+    <!--<div class="jumbotron text-center">
+      <h1>Mohiuddin Faruqe</h1>
+      <p>Project P1: System Setup / Portfolio</p>
+    </div>-->
+    <div class="container">
+      <div class="container-fluid bg-1 text-center bottom-buffer">
+           <h1>Project P2</h1>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+            <h2>Project P2</h2>
+            <h4><a href="p2.guddi.ca">View Project P2</a></h4>
+            <h4><a href="https://github.com/faruqem/p2">Project P2 Github Link</a></h4>
+            <p>The goal of this project is to give hands-on practice with working with the basics of PHP.</p>
+        </div>
+      </div>
+
+      <!--<nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Menu</a>
+          </div>
+        </div>
+      </nav>-->
+      <div class="container-fluid bg-1 text-center top-buffer">
+        <h3>Project P2</h3>
+        <h3>CSCI E-15 Dynamic Web Applications Course Projects</h3>
+        <h3>Harvard Extension School</h3>
+      </div>
+    </div>
+    <!--Local JS File-->
+    <script src="js/my_script.js"></script>
+    <!-- jQuery CDN -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <!-- Bootstrap Latest compiled and minified JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <body>
 </html>
